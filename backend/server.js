482ddd2be336541dc810945a06de285288b23578
JyotiSuman09 <jyotisumanswexpprac@gmail.com>
2022-10-09 14:03:16 +0530

    Commit to github

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
+const colors = require('colors')
 const { errorHandler } = require("./middleware/errorMiddleware");
 const dotenv = require("dotenv").config();
+const connectDB = require('./config/db')
 
 const PORT = process.env.PORT || 5000;
+
+connectDB()
 
 const app = express();
 
